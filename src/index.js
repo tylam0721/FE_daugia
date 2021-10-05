92fb56472ffda247b41d2070b26ef4684963add5
tylam0721 <lamnhatty2109@gmail.com>
2021-10-05 11:14:58 +0700

    refactored project

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import indexRoutes from './routes/index.jsx';
-import {  Route, Switch } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom'
-
-import './assets/scss/style.css';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { StateProvider } from "./StateProvider/StateProvider";
+import { InitialState, Reducer } from "./Reducer/Reducer";
 
 ReactDOM.render(
-  <HashRouter>
-  
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
-    </Switch>
-  
-  </HashRouter>
-  ,document.getElementById('root')); 
+  <StateProvider initialState={InitialState} reducer={Reducer}>
+    <App />
+  </StateProvider>,
+  document.getElementById("root")
+);
