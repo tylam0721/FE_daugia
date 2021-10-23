4540fbd7f342dc686d7271ffca22f45159db4325
tylam0721 <lamnhatty2109@gmail.com>
2021-10-24 01:17:57 +0700

    added sidebar

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -14,7 +14,6 @@ function Home() {
       setLoading(false);
     });
 
-    
 
 
     // setTimeout(() => {
---
+++ src/container/SideMenu/SideMenu.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './SideMenu.css';
+
+
+function SideMenu() {
+  return (
+    <div className="sidenav">
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Clients</a>
+      <a href="#">Contact</a>
+    </div>
+  )
+}
+
+export default SideMenu;
