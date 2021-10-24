c49e41f4e76eb27e4004a37d5b7766b9ac40d450
tylam0721 <lamnhatty2109@gmail.com>
2021-10-24 15:44:51 +0700

    Added paging

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -125,17 +125,20 @@ function Home() {
               ></Product>
             </Grid.Column> */}
           </Grid>
-          {/* <Pagination
-            boundaryRange={0}
-            defaultActivePage={1}
-            ellipsisItem={null}
-            firstItem={null}
-            lastItem={null}
-            siblingRange={1}
-            totalPages={10}
-          /> */}
         </Container>
       )}
+      <div className="paging">
+        <Pagination
+          boundaryRange={0}
+          defaultActivePage={1}
+          ellipsisItem={null}
+          firstItem={null}
+          lastItem={null}
+          siblingRange={1}
+          totalPages={10}
+        />
+      </div>
+
     </div>
   );
 }
