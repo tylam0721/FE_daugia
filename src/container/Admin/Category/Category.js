4a743f43573aa4d1fc03b29ca805dafeaa1843af
tdnam99 <tdnam.17ck1@gmail.com>
2021-10-24 14:49:07 +0700

    Feature: Admin page - category page

--- src/container/Admin/Category/Category.js
+++ src/container/Admin/Category/Category.js
@@ -1,91 +1,257 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Item, Card, Grid, Message } from "semantic-ui-react";
 import "./Category.css";
-import 'antd/dist/antd.css';
-import { Table, Tag, Space } from 'antd';;
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <a>{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: tags => (
-      <>
-        {tags.map(tag => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
+import "antd/dist/antd.css";
+import {
+  Table,
+  Tag,
+  Space,
+  Button,
+  Modal,
+  Input,
+  Row,
+  Col,
+  notification,
+} from "antd";
+import { API_HOST, API_HOST_DEV } from "../../../config/endpoints";
+import { NotificationTwoTone } from "@ant-design/icons";
+import axios from "axios";
 
 function Category() {
+  const [data, setdata] = useState([
+    {
+      Name: "nam",
+      DateCreated: "2021-10-24T00:00:00",
+    },
+  ]);
+
+  const [isModalVisibleAddCategory, setIsModalVisibleAddCategory] =
+    useState(false);
+  const [isModalVisibleDeleteCategory, setIsModalVisibleDeleteCategory] =
+    useState(false);
+
+  const [isModalVisibleUpdateCategory, setIsModalVisibleUpdateCategory] =
+    useState(false);
+  const [namcategory, setNamecategory] = useState();
+  const [namupdatecategory, setNameUpdatecategory] = useState();
+  const [selectedRow, setSeletedRow] = useState(null);
+
+  const AuthStr = "Bearer ".concat("");
+
+  let config = {
+    headers: {
+      Authorization: AuthStr,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const openNotificationWithIcon = (type) => {
+    notification[type]({
+      message: "Lỗi thao tác",
+      description: "Danh mục đã tồn tại sản phẩm không thể xóa",
+    });
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "Name",
+      key: "Name",
+      render: (text) => <p>{text}</p>,
+    },
+    {
+      title: "DateCreated",
+      dataIndex: "DateCreated",
+      key: "DateCreated",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => {
+        return (
+          <Space
+            size="middle"
+            onClick={() => {
+              setSeletedRow(record);
+              setNameUpdatecategory(record.Name);
+            }}
+          >
+            <Button type="primary" ghost onClick={showModalUpdateCategory}>
+              Update
+            </Button>
+            <Button
+              type="primary"
+              danger
+              ghost
+              onClick={showModalDeleteCategory}
+            >
+              Delete
+            </Button>
+          </Space>
+        );
+      },
+    },
+  ];
+
+  const showModalAddCategory = () => {
+    setIsModalVisibleAddCategory(true);
+  };
+
+  const handleCancelAddCategory = () => {
+    setIsModalVisibleAddCategory(false);
+  };
+
+  const handleOkAddCategory = () => {
+    setIsModalVisibleAddCategory(false);
+    let obcatebory = {
+      Name: namcategory,
+    };
+
+    axios
+      .post(
+        `${API_HOST_DEV}/api/category/add`,
+        {
+          body: obcatebory,
+        },
+        config
+      )
+      .then((response) => {
+        FnLoadCategory();
+      })
+      .catch((error) => {
+        console.log("error " + error);
+      });
+  };
+
+  const showModalDeleteCategory = () => {
+    setIsModalVisibleDeleteCategory(true);
+  };
+
+  const handleCancelDeleteCategory = () => {
+    setIsModalVisibleDeleteCategory(false);
+  };
+
+  const handleOkDeleteCategory = () => {
+    setIsModalVisibleDeleteCategory(false);
+    axios
+      .get(`${API_HOST_DEV}/api/category/delete/` + selectedRow.id, {}, config)
+      .then((response) => {
+        FnLoadCategory();
+      })
+      .catch((error) => {
+        console.log("error " + error);
+        openNotificationWithIcon("error");
+      });
+  };
+
+  const handleCancelUpdateCategory = () => {
+    setIsModalVisibleUpdateCategory(false);
+  };
+
+  const handleOkUpdateCategory = () => {
+    setIsModalVisibleUpdateCategory(false);
+
+    axios
+      .post(
+        `${API_HOST_DEV}/api/category/update`,
+        {
+          Name: namupdatecategory,
+          id: selectedRow.id,
+        },
+        config
+      )
+      .then((response) => {
+        FnLoadCategory();
+      })
+      .catch((error) => {
+        console.log("error " + error);
+      });
+  };
+
+  const showModalUpdateCategory = () => {
+    setIsModalVisibleUpdateCategory(true);
+  };
+
+  let FnLoadCategory = () => {
+    axios
+      .get(`${API_HOST_DEV}/api/category`, {
+        headers: { Authorization: AuthStr },
+      })
+      .then((response) => {
+        setdata(response.data.data);
+      })
+      .catch((error) => {
+        console.log("error " + error);
+      });
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    FnLoadCategory();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className="checkout">
-      <Container>
-        <div className="div-parent">
-        <Table columns={columns} dataSource={data} /> 
+    <>
+      <div className="div__Content">
+        <div className="div__Alert">
+          <Row>
+            <Col span={16}></Col>
+            <Col span={8}></Col>
+          </Row>
         </div>
-      </Container>
-    </div>
+
+        <Container>
+          <div className="div__parent">
+            <Button type="primary" onClick={showModalAddCategory}>
+              Add Category
+            </Button>
+            <Table
+              pagination={{ pageSize: 4 }}
+              columns={columns}
+              dataSource={data}
+            />
+          </div>
+        </Container>
+        <Modal
+          title="ADD CATEGORY"
+          visible={isModalVisibleAddCategory}
+          onOk={handleOkAddCategory}
+          onCancel={handleCancelAddCategory}
+        >
+          <p>Name Category</p>
+          <Input
+            value={namcategory}
+            placeholder="Basic usage"
+            onChange={(e) => setNamecategory(e.target.value)}
+          />
+        </Modal>
+        <Modal
+          title="DELETE CATEGORY"
+          visible={isModalVisibleDeleteCategory}
+          onOk={handleOkDeleteCategory}
+          onCancel={handleCancelDeleteCategory}
+        >
+          <h1>
+            <b className="delete__category">Bạn có chắc muốn xóa </b>{" "}
+            <NotificationTwoTone />{" "}
+          </h1>
+        </Modal>
+        <Modal
+          title="UPDATE CATEGORY"
+          visible={isModalVisibleUpdateCategory}
+          onOk={handleOkUpdateCategory}
+          onCancel={handleCancelUpdateCategory}
+        >
+          <p>Name Category</p>
+          <Input
+            value={namupdatecategory}
+            placeholder="Basic usage"
+            onChange={(e) => setNameUpdatecategory(e.target.value)}
+          />
+        </Modal>
+      </div>
+    </>
   );
 }
 
