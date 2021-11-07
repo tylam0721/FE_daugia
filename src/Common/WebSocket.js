48554c24e0efb78598b3862e606067a429af71bd
tdnam99 <tdnam.17ck1@gmail.com>
2021-11-07 20:17:22 +0700

    write logs

--- src/Common/WebSocket.js
+++ src/Common/WebSocket.js
@@ -1,4 +1,4 @@
 const PORT = process.env.PORT || 4000;
-//const webSocket = new WebSocket(`ws://auction-nhom5.herokuapp.com`);
-const webSocket = new WebSocket(`ws://localhost:4000`);
+const webSocket = new WebSocket(`ws://auction-nhom5.herokuapp.com`);
+//const webSocket = new WebSocket(`ws://localhost:4000`);
 export default webSocket;
